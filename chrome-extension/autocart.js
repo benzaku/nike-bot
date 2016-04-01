449f356da3f4cab29410b4700e38df9ff21843b6
Haiyang Xu <haiyang.xu@citrix.com>
2016-04-01 15:57:29 +0200

    Add some customization

--- chrome-extension/autocart.js
+++ chrome-extension/autocart.js
@@ -1,22 +1,27 @@
-var size = "12";
+var size = "EU 41";
+var preferedSizes = ["EU 41", "EU 40.5", "EU 42", "EU 42.5", "EU 43", "EU 44", "EU 44.5", "EU 44.5", "EU 45"];
 var amount = 1;
 
 function addToCart() {
 	var sizesList=document.getElementsByName("skuAndSize")[0];
+	//console.log(sizesList);
 	function setQuantity() {
 		document.getElementsByName("qty")[0].selectedIndex = amount-1;
 	}
 	function setSizeValue() {
 		for (var i=0; i<sizesList.length; i++){
-			if(sizesList.options[i].text == size) {
-				document.getElementsByName("skuAndSize")[0].selectedIndex = i;
-				setQuantity();
+			for(var j = 0; j < preferedSizes.length; ++j) {
+				if (sizesList.options[i].text == preferedSizes[j]) {
+						document.getElementsByName("skuAndSize")[0].selectedIndex = i;
+						setQuantity();
+				}
 			}
 		}
 	}
 	if(sizesList != undefined) {
 	    setSizeValue();
-	    document.getElementsByClassName("button-container add-to-cart")[0].click();
+	    //document.getElementsByClassName("js-add-to-cart add-to-cart nsg-button nsg-grad--nike-orange")[0].click();
+			document.getElementById("buyingtools-add-to-cart-button").click();
 	} else {
 	    setTimeout("addToCart()", 250);
 	}
